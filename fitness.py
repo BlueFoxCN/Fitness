6f98cc19cd45bb723cb653733a546e7dccb59deb
JesseYang <jesse.yang1985@gmail.com>
2018-05-15 14:00:33 +0800

    tidyup code

--- fitness.py
+++ fitness.py
@@ -22,24 +22,11 @@ if __name__ == "__main__":
     enable_capture = Event()
     enable_predict = Event()
 
-    # capture_thread = CaptureThread(capture_queue, enable_capture, video_file="20180513_1.mp4")
     capture_thread = CaptureThread(capture_queue, enable_capture)
     capture_thread.start()
 
     detect_thread = DetectThread(capture_queue, result_queue, enable_capture, enable_predict)
     detect_thread.start()
 
-    visualize_thread = VisualizeThread(result_queue, enable_predict, visualize_queue, output_path="output_2.mp4")
+    visualize_thread = VisualizeThread(result_queue, enable_predict, visualize_queue, output_path="output.mp4")
     visualize_thread.start()
-
-    '''
-    while True:
-        frame = visualize_queue.get()
-        # frame = np.random.rand(500, 800, 3)
-        print(frame.shape)
-        cv2.imshow('frame', frame)
-        if cv2.waitKey(10) & 0xFF == ord('q'):
-            break
-
-    cv2.destroyAllWindows()
-    '''
